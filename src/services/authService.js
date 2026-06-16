5bd9b795d223433c2ad6a637458756f7fe94e807
Raana <raanaafkari96@gmail.com>
2026-06-16 11:16:54 +0200

    feat: add authService and connect login form

---
+++ src/services/authService.js
@@ -0,0 +1,19 @@
+export const authService = {
+  login(email, password) {
+    // şimdilik mock (sahte login)
+    if (email === "test@test.com" && password === "123456") {
+      return {
+        success: true,
+        token: "fake-jwt-token",
+        user: {
+          email
+        }
+      }
+    }
+
+    return {
+      success: false,
+      message: "Invalid credentials"
+    }
+  }
+}
