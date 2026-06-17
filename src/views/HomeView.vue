3866e7c9cf7aa4945f0de039cb38498bf46cfdd1
andreaperezgon <andreaperezgon@gmail.com>
2026-06-17 10:15:24 +0200

    agregar datos de prueba a la tarjeta

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <script setup>
 import TarjetasPersonajes from '../componentes/tarjetaspersonajes.vue'
+
+const personajePrueba = {
+  name: 'Baloo',
+  imageUrl: 'https://static.wikia.nocookie.net/disney/images/0/03/Profile_-_Baloo.jpeg'
+}
 </script>
 
 <template>
   <main>
     <h1>Disney Characters</h1>
 
-    <TarjetasPersonajes />
+    <TarjetasPersonajes :personaje="personajePrueba" />
   </main>
 </template>
