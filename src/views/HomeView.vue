94fd376cd7c050feffaf5b66cb3ab439df381c84
andreaperezgon <andreaperezgon@gmail.com>
2026-06-17 10:21:22 +0200

    conecta la API de disney

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,27 @@
 <script setup>
+import { ref, onMounted } from 'vue'
 import TarjetasPersonajes from '../componentes/tarjetaspersonajes.vue'
 
-const personajes = [
-  {
-    id: 1,
-    name: 'Baloo',
-    imageUrl: 'https://static.wikia.nocookie.net/disney/images/0/03/Profile_-_Baloo.jpeg'
-  },
-  {
-    id: 2,
-    name: 'Mickey Mouse',
-    imageUrl: 'https://picsum.photos/300/400?1'
-  },
-  {
-    id: 3,
-    name: 'Donald Duck',
-    imageUrl: 'https://picsum.photos/300/400?2'
-  }
-]
+const personajes = ref([])
+
+onMounted(async () => {
+  const response = await fetch('https://api.disneyapi.dev/character')
+  const data = await response.json()
+
+  personajes.value = data.data
+    .filter(personaje => personaje.imageUrl)
+    .slice(0, 10)
+})
 </script>
 
 <template>
   <main>
     <h1>Disney Characters</h1>
 
-    <TarjetasPersonajes :personaje="personajePrueba" /><TarjetasPersonajes
-  v-for="personaje in personajes"
-  :key="personaje.id"
-  :personaje="personaje"
-/>  </main>
+    <TarjetasPersonajes
+      v-for="personaje in personajes"
+      :key="personaje._id"
+      :personaje="personaje"
+    />
+  </main>
 </template>
