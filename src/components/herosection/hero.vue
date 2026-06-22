717fc08c4149ad35b90684185e8a2fbc88453e9c
andreaperezgon <andreaperezgon@gmail.com>
2026-06-22 10:31:38 +0200

    agregar imagen de fondo al hero

--- src/components/herosection/hero.vue
+++ src/components/herosection/hero.vue
@@ -1,7 +1,11 @@
 <template>
-  <section
-    class="relative h-125 overflow-hidden rounded-3xl mb-12"
-  >
+  <section class="relative h-125 overflow-hidden rounded-3xl mb-12">
+    <img
+      src="https://images.unsplash.com/photo-1518709268805-4e9042af2176"
+      alt="Paisaje mágico"
+      class="absolute inset-0 h-full w-full object-cover"
+    />
+
     <div class="hero-content">
       <h2>Descubre la magia de cada personaje</h2>
 
