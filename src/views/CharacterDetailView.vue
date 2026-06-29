94e0e36d576c37ac619a2440a7786162e63a3e60
andreaperezgon <andreaperezgon@gmail.com>
2026-06-29 22:28:31 +0200

    feat: crear vista de detalle del personaje

---
+++ src/views/CharacterDetailView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <main class="min-h-screen bg-slate-950 p-8 text-white">
+    <h1 class="text-4xl font-bold">
+      Character Detail
+    </h1>
+  </main>
+</template>
