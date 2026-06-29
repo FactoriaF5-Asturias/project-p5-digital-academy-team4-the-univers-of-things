25d25e3ff94df8eee50fc51225583531836eceef
andreaperezgon <andreaperezgon@gmail.com>
2026-06-29 22:37:12 +0200

    feat: añadir navegación desde tarjetas al detalle

--- src/components/cards/tarjetaspersonajes.vue
+++ src/components/cards/tarjetaspersonajes.vue
@@ -1,17 +1,22 @@
 <template>
-  <article class="overflow-hidden rounded-2xl bg-slate-900 shadow-lg transition hover:-translate-y-1 hover:shadow-2xl">
-    <img
-      :src="personaje.imageUrl"
-      :alt="personaje.name"
-      class="h-60 w-full object-cover"
-    >
+  <RouterLink
+    :to="`/character/${personaje._id}`"
+    class="block"
+  >
+    <article class="overflow-hidden rounded-2xl bg-slate-900 shadow-lg transition hover:-translate-y-1 hover:shadow-2xl">
+      <img
+        :src="personaje.imageUrl"
+        :alt="personaje.name"
+        class="h-60 w-full object-cover"
+      >
 
-    <div class="p-4">
-      <h3 class="text-center text-lg font-semibold text-white">
-        {{ personaje.name }}
-      </h3>
-    </div>
-  </article>
+      <div class="p-4">
+        <h3 class="text-center text-lg font-semibold text-white">
+          {{ personaje.name }}
+        </h3>
+      </div>
+    </article>
+  </RouterLink>
 </template>
 
 <script setup>
